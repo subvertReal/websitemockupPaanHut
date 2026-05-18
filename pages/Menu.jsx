c1b308ebcbca8f2594e1d0f0c4ba7f30b9a3467e
subvertReal <sherjeelk2004@gmail.com>
2026-05-18 13:36:52 -0400

    working on making a fetch to json for data so that it can be changed through external means

--- pages/Menu.jsx
+++ pages/Menu.jsx
@@ -1,7 +1,42 @@
+import { Row, Container, Col } from "react-bootstrap";
 import NavBar from "../components/NavBar";
+import { useEffect,useState } from "react";
+import axios from 'axios';
 
 function Menu() {
-    
+    const [json, setJson] = useState();
+
+    async function fetchJson(){
+        try{
+        let url = 'https://raw.githubusercontent.com/subvertReal/dataRepoForMockups/refs/heads/main/paanhut/menu.json';
+        await axios.get(url)
+            .then(response=>{
+                let res = response.data;
+                let arr = [];
+                for (const key in res){
+                    
+                    arr.push(key);
+                    for (const key2 in res){
+                        
+                    }
+                    console.log(`log ${key}: ${res[key]}`)
+
+                }
+                console.log(arr);
+                
+
+            }
+            )
+        }
+        catch(err){
+            console.error(err);
+        }
+        
+    }
+    useEffect(()=>{
+        fetchJson();
+
+    })
 
     return (
         <div>
@@ -10,6 +45,18 @@ function Menu() {
 
             <NavBar />
             <p>This is a menu</p>
+            <Container>
+                <Row className='text-center'>
+                    <Col>
+                        Text
+                    </Col>
+                    <Col>
+                        Text
+                    </Col>
+                </Row>
+                
+
+            </Container>
 
 
       </div>
